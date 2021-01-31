92d7ca38c1ad7c2c584f61023cd2fab1dd6975e1
Henry Reynaud <hreyna1@lsu.edu>
2021-01-31 16:24:19 -0600

    change land to end

--- tellomain.py
+++ tellomain.py
@@ -1,10 +1,13 @@
-from djitellopy import Tello
+import time
 import cv2
+# from Aruco import tellopy
+from djitellopy import Tello
+
 
 width = 320
 height = 240
 startCounter = 0
-
+print("Starts Connecting:", time.process_time())
 # initialization
 me = Tello()
 me.connect()
@@ -14,33 +17,50 @@ me.up_down_velocity = 0
 me.yaw_velocity = 0
 me.speed = 0
 
+print("Done Connecting:", time.process_time())
+print("Battery:", me.get_battery())
 
-print(me.get_battery())
 
-
-me.streamoff()
-me.streamon()
+# me.streamoff()
+# me.streamon()
+#camera = me.get_video_capture()
 
 while True:
-    frame_read = me.get_frame_read()
-    myFrame = frame_read.frame
-    img = cv2.resize(myFrame, (width, height))
-    cv2.imshow("The Camera", img)
-
-    if startCounter == 0:
-        me.takeoff()
-        print(me.get_height())
-        me.move_back(40)
-        # me.move_right(20)
-        me.rotate_clockwise(180)
-        me.rotate_counter_clockwise(180)
-        me.move_forward(40)
-        startCounter = 1
+    #_, imageFrame = camera.read()
+    # frame_read = me.get_frame_read()
+    # myFrame = frame_read.frame
+    # img = cv2.resize(myFrame, (width, height))
+    # cv2.imshow("The Camera", img)
+    #cv2.imshow("Drone Camera", imageFrame)
+    """
+    # if startCounter == 0:
+    me.takeoff()
+    print(me.get_height())
+    # me.move_back(40)
+    # me.move_right(20)
+    me.move_up(65)
+    print(me.get_height())
+    me.move_forward(153)
+    # me.rotate_counter_clockwise(180)
+    # me.move_forward(40)
+    #    startCounter = 1
 
     # cv2.imshow("The Camera", img)
+"""
+    print("Beginning Takeoff:", time.process_time())
+    me.takeoff()
+    print("Done Takeoff:", time.process_time())
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        me.streamoff()
-        me.land()
+    print("Beginning Move:", time.process_time())
+    me.move_forward(100)
+    print("Done Move:", time.process_time())
+
+    print("Beginning Move Back:", time.process_time())
+    me.move_back(100)
+    print("Done Move Back:", time.process_time())
+
+    me.stop()
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        # cv2.destroyAllWindows()
+        me.end()
         break
